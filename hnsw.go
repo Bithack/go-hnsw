75ebd026964088de339f20a1ab52254428067fa8
Oscar Franzen <oscar.franzen@appetin.se>
2017-03-15 21:41:56 +0100

    fixed import paths

--- hnsw.go
+++ hnsw.go
@@ -6,9 +6,9 @@ import (
 	"math/rand"
 	"sync"
 
-	"./bitsetpool"
-	"./distqueue"
-	"./f32"
+	"github.com/Bithack/go-hnsw/bitsetpool"
+	"github.com/Bithack/go-hnsw/distqueue"
+	"github.com/Bithack/go-hnsw/f32"
 )
 
 type Point []float32
